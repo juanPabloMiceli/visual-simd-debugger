aa993550e5e137b94d7b9c247ef8c324f0e1247c
juampi_miceli <micelijuanpablo@gmail.com>
2021-07-27 15:59:57 -0300

    BPF filter more commented

--- backend/microjail/microjail.c
+++ backend/microjail/microjail.c
@@ -19,38 +19,42 @@ static int install_syscall_filter(char *msg, u_int64_t *randomNumbers)
 
     struct sock_filter filter[] = {
         VALIDATE_ARCHITECTURE,
-        EXAMINE_SYSCALL,
+
+        //Load syscall
+        BPF_STMT(BPF_LD + BPF_W + BPF_ABS, syscall_nr), //Load syscall number to W
+
         //Exit
-        ALLOW_SYSCALL(exit),
+        /*1*/ BPF_JUMP(BPF_JMP + BPF_JEQ + BPF_K, __NR_exit, 0, 1), //if syscall is exit, don't jump. Else, jump to statement 3.
+        /*2*/ BPF_STMT(BPF_RET + BPF_K, SECCOMP_RET_ALLOW),         //Allow syscall. (exit is always allowed)
 
         ///Execve
-        /*1*/ BPF_JUMP(BPF_JMP + BPF_JEQ + BPF_K, __NR_execve, 0, 2),    //if syscall is execve, don't jump. Else, jump to statement 4.
-        /*2*/ BPF_STMT(BPF_LD + BPF_W + BPF_ABS, syscall_arg(0)),        //Load arg0 to W
-        /*3*/ BPF_JUMP(BPF_JMP + BPF_JEQ + BPF_K, (u_int64_t)msg, 1, 0), //If arg0 == msg => jump to statement 5. (first check passed)
-        /*4*/ BPF_STMT(BPF_RET + BPF_K, SECCOMP_RET_KILL),               //Kill process. Invalid arg0
+        /*3*/ BPF_JUMP(BPF_JMP + BPF_JEQ + BPF_K, __NR_execve, 0, 19),   //if syscall is execve, don't jump. Else, jump to statement 23. (there could be another syscall to inspect)
+        /*4*/ BPF_STMT(BPF_LD + BPF_W + BPF_ABS, syscall_arg(0)),        //Load arg0 to W
+        /*5*/ BPF_JUMP(BPF_JMP + BPF_JEQ + BPF_K, (u_int64_t)msg, 1, 0), //If arg0 == msg => jump to statement 7. (first check passed)
+        /*6*/ BPF_STMT(BPF_RET + BPF_K, SECCOMP_RET_KILL),               //Kill process. Invalid arg0
 
-        /*5*/ BPF_STMT(BPF_LD + BPF_W + BPF_ABS, syscall_arg(1)),         //Load arg1 to W
-        /*6*/ BPF_JUMP(BPF_JMP + BPF_JEQ + BPF_K, (u_int64_t)NULL, 1, 0), //If arg1 == NULL => jump to statement 8. (second check passed)
-        /*7*/ BPF_STMT(BPF_RET + BPF_K, SECCOMP_RET_KILL),                //Kill process. Invalid arg1
+        /*7*/ BPF_STMT(BPF_LD + BPF_W + BPF_ABS, syscall_arg(1)),         //Load arg1 to W
+        /*8*/ BPF_JUMP(BPF_JMP + BPF_JEQ + BPF_K, (u_int64_t)NULL, 1, 0), //If arg1 == NULL => jump to statement 10. (second check passed)
+        /*9*/ BPF_STMT(BPF_RET + BPF_K, SECCOMP_RET_KILL),                //Kill process. Invalid arg1
 
-        /*8*/ BPF_STMT(BPF_LD + BPF_W + BPF_ABS, syscall_arg(2)),         //Load arg2 to W
-        /*9*/ BPF_JUMP(BPF_JMP + BPF_JEQ + BPF_K, (u_int64_t)NULL, 1, 0), //If arg0 == msg => jump to statement 11. (third check passed)
-        /*10*/ BPF_STMT(BPF_RET + BPF_K, SECCOMP_RET_KILL),               //Kill process. Invalid arg2
+        /*10*/ BPF_STMT(BPF_LD + BPF_W + BPF_ABS, syscall_arg(2)),         //Load arg2 to W
+        /*11*/ BPF_JUMP(BPF_JMP + BPF_JEQ + BPF_K, (u_int64_t)NULL, 1, 0), //If arg2 == msg => jump to statement 13. (third check passed)
+        /*12*/ BPF_STMT(BPF_RET + BPF_K, SECCOMP_RET_KILL),                //Kill process. Invalid arg2
 
-        /*11*/ BPF_STMT(BPF_LD + BPF_W + BPF_ABS, syscall_arg(3)),                     //Load arg3 to W
-        /*12*/ BPF_JUMP(BPF_JMP + BPF_JEQ + BPF_K, (u_int64_t)randomNumbers[0], 1, 0), //If arg3 == random1 => jump to statement 14. (fourth check passed)
-        /*13*/ BPF_STMT(BPF_RET + BPF_K, SECCOMP_RET_KILL),                            //Kill process. Invalid arg3
+        /*13*/ BPF_STMT(BPF_LD + BPF_W + BPF_ABS, syscall_arg(3)),                     //Load arg3 to W
+        /*14*/ BPF_JUMP(BPF_JMP + BPF_JEQ + BPF_K, (u_int64_t)randomNumbers[0], 1, 0), //If arg3 == random1 => jump to statement 16. (fourth check passed)
+        /*15*/ BPF_STMT(BPF_RET + BPF_K, SECCOMP_RET_KILL),                            //Kill process. Invalid arg3
 
-        /*14*/ BPF_STMT(BPF_LD + BPF_W + BPF_ABS, syscall_arg(4)),                     //Load arg4 to W
-        /*15*/ BPF_JUMP(BPF_JMP + BPF_JEQ + BPF_K, (u_int64_t)randomNumbers[1], 1, 0), //If arg4 = random2 => jump to statement 17. (fifth check passed)
-        /*16*/ BPF_STMT(BPF_RET + BPF_K, SECCOMP_RET_KILL),                            //Kill process. Invalid arg4
+        /*16*/ BPF_STMT(BPF_LD + BPF_W + BPF_ABS, syscall_arg(4)),                     //Load arg4 to W
+        /*17*/ BPF_JUMP(BPF_JMP + BPF_JEQ + BPF_K, (u_int64_t)randomNumbers[1], 1, 0), //If arg4 = random2 => jump to statement 19. (fifth check passed)
+        /*18*/ BPF_STMT(BPF_RET + BPF_K, SECCOMP_RET_KILL),                            //Kill process. Invalid arg4
 
-        /*17*/ BPF_STMT(BPF_LD + BPF_W + BPF_ABS, syscall_arg(5)),                     //Load arg5 to W
-        /*18*/ BPF_JUMP(BPF_JMP + BPF_JEQ + BPF_K, (u_int64_t)randomNumbers[2], 1, 0), //If arg5 = random3 => jump to statement 20. (all checks passed. Syscall is allowed)
-        /*19*/ BPF_STMT(BPF_RET + BPF_K, SECCOMP_RET_KILL),                            //Kill process. Invalid arg5
+        /*19*/ BPF_STMT(BPF_LD + BPF_W + BPF_ABS, syscall_arg(5)),                     //Load arg5 to W
+        /*20*/ BPF_JUMP(BPF_JMP + BPF_JEQ + BPF_K, (u_int64_t)randomNumbers[2], 1, 0), //If arg5 = random3 => jump to statement 22. (all checks passed. Syscall is allowed)
+        /*21*/ BPF_STMT(BPF_RET + BPF_K, SECCOMP_RET_KILL),                            //Kill process. Invalid arg5
 
-        /*20*/ BPF_STMT(BPF_RET + BPF_K, SECCOMP_RET_ALLOW), //Allow syscall
-        /*21*/ KILL_PROCESS                                  //Kill process
+        /*22*/ BPF_STMT(BPF_RET + BPF_K, SECCOMP_RET_ALLOW), //Allow syscall
+        /*23*/ KILL_PROCESS                                  //Kill process
 
     };
 
